73c41388d8d331011c16f5ca0c9f03224e62c5b2
Yusuke Shibata <yusukeshib@gmail.com>
2024-11-14 09:20:10 -0500

    DarkMode refactoring

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,9 @@
 import * as React from "react"
 import { Helmet } from "react-helmet";
 import { Link } from "gatsby"
-import { useAtom } from 'jotai'
-import { createGlobalStyle } from 'styled-components'
-import { darkModeAtom, DarkModeButton } from './theme'
-
-const DarkMode = createGlobalStyle`
-body {
-  font-family: times, serif;
-  background: ${props => props.dark ? '#222' : '#fff'};
-  color: ${props => props.dark ? '#fff' : '#000'};
-}
-a {
-  color: ${props => props.dark ? '#99f' : 'auto'};
-}
-`
+import { ThemeStyles, DarkModeButton } from './theme'
 
 const Layout = ({ author, location, title, children }) => {
-  const [darkMode] = useAtom(darkModeAtom)
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
@@ -41,8 +27,8 @@ const Layout = ({ author, location, title, children }) => {
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <ThemeStyles />
       <Helmet htmlAttributes={{ lang: 'ja', }} />
-      <DarkMode dark={darkMode} />
       <header className="global-header">{header}</header>
       <main>{children}</main>
     </div>
--- src/components/theme.js
+++ src/components/theme.js
@@ -1,13 +1,29 @@
 import * as React from 'react'
 import { useAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const darkModeAtom = atomWithStorage('darkMode', false)
 
 const Button = styled.a`
   cursor: pointer;
 `
 
-export const darkModeAtom = atomWithStorage('darkMode', false)
+const DarkMode = createGlobalStyle`
+body {
+  font-family: times, serif;
+  background: ${props => props.dark ? '#111' : '#fff'};
+  color: ${props => props.dark ? '#fff' : '#000'};
+}
+a {
+  color: ${props => props.dark ? '#fff' : 'auto'};
+}
+`
+
+export const ThemeStyles = () => {
+  const [darkMode] = useAtom(darkModeAtom)
+  return <DarkMode dark={darkMode} />
+}
 
 export const DarkModeButton = () => {
   const [darkMode, setDarkMode] = useAtom(darkModeAtom)
