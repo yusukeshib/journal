2f06cbb1711d6dc4766a4c979175f713c6547379
Yusuke Shibata <yusukeshib@gmail.com>
2024-11-21 12:40:56 -0500

    Remove styled-components

--- src/components/collapsable.jsx
+++ src/components/collapsable.jsx
@@ -1,10 +1,4 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
-
-const A = styled.a`
-  text-decoration: underline;
-  cursor: pointer;
-`
 
 export const Collapsable = function Collapsable({ children }) {
   const [opened, setOpened] = useState(false);
@@ -12,11 +6,11 @@ export const Collapsable = function Collapsable({ children }) {
   if (opened) {
     return <>
       <div>
-        <A onClick={() => setOpened(false)}>コメント欄を隠す</A>
+        <a href="javascript:void(0)" onClick={() => setOpened(false)}>コメント欄を隠す</a>
       </div >
       {children}
     </>
   } else {
-    return <A onClick={() => setOpened(true)}>コメントを見る</A>
+    return <a href="javascript:void(0)" onClick={() => setOpened(true)}>コメントを見る</a>
   }
 }
--- src/components/theme.js
+++ src/components/theme.js
@@ -1,20 +1,14 @@
 import * as React from 'react'
 import { useAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
-import styled from 'styled-components'
 
 export const darkModeAtom = atomWithStorage('darkMode', false)
-
-const Button = styled.a`
-  cursor: pointer;
-`
 
 export const DarkModeButton = () => {
   const [darkMode, setDarkMode] = useAtom(darkModeAtom)
   const toggleDarkMode = () => setDarkMode(!darkMode)
-
   return (
-    <Button onClick={toggleDarkMode}>{!darkMode ? '☀️' : '🌙'}</Button>
+    <a href="javascript:void(0)" onClick={toggleDarkMode}>{!darkMode ? '☀️' : '🌙'}</a>
   )
 }
 
