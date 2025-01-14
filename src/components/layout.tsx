9de5f25042272bec720927d02df9c9d0af1dde26
Yusuke Shibata <yusukeshib@gmail.com>
2025-01-13 21:31:22 -0500

    Switch to styled-components

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,17 @@
 import * as React from "react"
+import { styled, createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+body {
+  font-size: 16px;
+  font-family: times, serif;
+  padding: 0;
+  margin: 0;
+}
+ul {
+  padding-inline-start: 1rem;
+}
+`
 
 export function Layout({
   location,
@@ -11,11 +24,27 @@ export function Layout({
   const isRootPath = location.pathname === rootPath
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <h1 className="main-heading">
-        <a href="/">Yusuke Shibata's portfolio</a>
-      </h1>
-      <main className="main">{children}</main>
-    </div>
+    <>
+      <GlobalStyle />
+      <Container data-is-root-path={isRootPath}>
+        <Title>
+          <a href="/">Yusuke Shibata's portfolio</a>
+        </Title>
+        <main className="main">{children}</main>
+      </Container>
+    </>
   )
 }
+
+const Container = styled.div`
+  padding: 1rem;
+  background: #fff;
+  color: #000;
+  max-width: 720px;
+`
+const Title = styled.h1`
+  font-size: 1rem;
+  font-style: italic;
+  font-weight: normal;
+  margin: 0;
+`
